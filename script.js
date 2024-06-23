efbcdea578cdcdf0b8413c3a11eb38b470e09501
Andersonfrota1988 <143961469+Andersonfrota1988@users.noreply.github.com>
2024-06-23 16:38:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+// Aula 08
+
+const button = document.querySelector('#button')
+
+button.addEventListener('click', 
+function () {
+    document.querySelector("#h1").classList.toggle("oculto")
+   
+    
+} )
+
+
